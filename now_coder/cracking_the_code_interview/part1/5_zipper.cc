2b18c180757b4945dd4302589c627a33f7b79b98
countryhu <countryhu@163.com>
2017-10-12 15:07:48 +0800

    5_zipper

--- now_coder/cracking_the_code_interview/part1/5_zipper.cc
+++ now_coder/cracking_the_code_interview/part1/5_zipper.cc
@@ -107,47 +107,47 @@ class Zipper {
   }
 };
 
-int main() {
-  Zipper obj;
-  // 测试样例1
-  if (obj.zipString("aabcccccaaa") == "a2b1c5a3") {
-    cout << "case1: " << "\"aabcccccaaa\" check ok" << endl;
-  }
+// int main() {
+  // Zipper obj;
+  // // 测试样例1
+  // if (obj.zipString("aabcccccaaa") == "a2b1c5a3") {
+    // cout << "case1: " << "\"aabcccccaaa\" check ok" << endl;
+  // }
 
-  // 测试样例2
-  if (obj.zipString("welcometonowcoderrrrr") == "welcometonowcoderrrrr") {
-    cout << "case2: " << "\"welcometonowcoderrrrr\" check ok" << endl;
-  }
+  // // 测试样例2
+  // if (obj.zipString("welcometonowcoderrrrr") == "welcometonowcoderrrrr") {
+    // cout << "case2: " << "\"welcometonowcoderrrrr\" check ok" << endl;
+  // }
 
-  // 特殊：空
-  if (obj.zipString("") == "") {
-    cout << "case3: " << "null check ok" << endl;
-  }
+  // // 特殊：空
+  // if (obj.zipString("") == "") {
+    // cout << "case3: " << "null check ok" << endl;
+  // }
 
-  // 特殊：1
-  if (obj.zipString("a") == "a") {
-    cout << "case4: " << "1 check ok" << endl;
-  }
+  // // 特殊：1
+  // if (obj.zipString("a") == "a") {
+    // cout << "case4: " << "1 check ok" << endl;
+  // }
 
-  // 特殊：长度大一点
-  if (obj.zipString("aaa") == "a3") {
-    cout << "case5: " << "\"1 \" check ok" << endl;
-  }
+  // // 特殊：长度大一点
+  // if (obj.zipString("aaa") == "a3") {
+    // cout << "case5: " << "\"1 \" check ok" << endl;
+  // }
 
-  // 特殊：长度相等
-  if (obj.zipString("aa") == "aa") {
-    cout << "case5: " << "\"1 \" check ok" << endl;
-  }
+  // // 特殊：长度相等
+  // if (obj.zipString("aa") == "aa") {
+    // cout << "case5: " << "\"1 \" check ok" << endl;
+  // }
 
-  // 特殊：失败用例
-  string in = "jjjjjjxxxxxxxooZLLLLLLLLQQQQQQQQQLLLLLLLLECXXXXXXXIIIIIIIIIhjjyyySSooooooooommmuuEEEEEEEEEnnnnnnnffffffAAAAAllllllllbbbbkkkkkkkkkkkkKKKKKKhhhhhhhhhooooooooooYCCCCCCOOOOOOOOOMMMMMMMMMMiiiiiivvvvvvvWWWWkkkkkkwwwwwwwMmmmmmmmmLLLwwwwwwwkkkjjjjjjttttQQQQQQQQQaaaaaaaFFFFFFFlllllllllggggggggggPPPPPPuuuuuuuuaYYYYYYwQQQFFFFFFFFFFaaaaapXXXXXXXxxxxxxQQQQQQQQQsssssGGGGfffffffddddddpppQQQQQQHHHTTTaaaaaaGGGGGGddyyyyyMhhllllllllllNNNNNNNNUUUWWWWWWLLLLLLLLLYYYYYYYYYYTTKKKKKKKKiiiiiiitttttttXXXXXXXXXLLLHZZZZZZZssssjjJJJEEEEEOOOOOttttttttttBBttttttTTTTTTTTTTrrrrttttRRRRRyyooooooaaaaaaaaarrrrrrrPPPPPPPjjPPPPddddddddddHHHHnnnnnnnnnnSSSSSSSSSSzzHHHHHHHHHddddddDDDzzzhhhhhfffffffffftttttteeeeeeeeEEEEEEEEEaaaaaaccccccccccFFFFFFFF";
-  string out = obj.zipString(in);
-  string expect = "j6x7o2Z1L8Q9L8E1C1X7I9h1j2y3S2o9m3u2E9n7f6A5l8b4k12K6h9o10Y1C6O9M10i6v7W4k6w7M1m8L3w7k3j6t4Q9a7F7l9g10P6u8a1Y6w1Q3F10a5p1X7x6Q9s5G4f7d6p3Q6H3T3a6G6d2y5M1h2l10N8U3W6L9Y10T2K8i7t7X9L3H1Z7s4j2J3E5O5t10B2t6T10r4t4R5y2o6a9r7P7j2P4d10H4n10S10z2H9d6D3z3h5f10t6e8E9a6c10F8";
-  if (out == expect) {
-    cout << "case5: check ok. with input:" << in << endl;
-  } else {
-    cout << "case5: check failed. with input:" << in << "\nexpect output:" << expect << "\nactual output:" << out << endl;
-  }
+  // // 特殊：失败用例
+  // string in = "jjjjjjxxxxxxxooZLLLLLLLLQQQQQQQQQLLLLLLLLECXXXXXXXIIIIIIIIIhjjyyySSooooooooommmuuEEEEEEEEEnnnnnnnffffffAAAAAllllllllbbbbkkkkkkkkkkkkKKKKKKhhhhhhhhhooooooooooYCCCCCCOOOOOOOOOMMMMMMMMMMiiiiiivvvvvvvWWWWkkkkkkwwwwwwwMmmmmmmmmLLLwwwwwwwkkkjjjjjjttttQQQQQQQQQaaaaaaaFFFFFFFlllllllllggggggggggPPPPPPuuuuuuuuaYYYYYYwQQQFFFFFFFFFFaaaaapXXXXXXXxxxxxxQQQQQQQQQsssssGGGGfffffffddddddpppQQQQQQHHHTTTaaaaaaGGGGGGddyyyyyMhhllllllllllNNNNNNNNUUUWWWWWWLLLLLLLLLYYYYYYYYYYTTKKKKKKKKiiiiiiitttttttXXXXXXXXXLLLHZZZZZZZssssjjJJJEEEEEOOOOOttttttttttBBttttttTTTTTTTTTTrrrrttttRRRRRyyooooooaaaaaaaaarrrrrrrPPPPPPPjjPPPPddddddddddHHHHnnnnnnnnnnSSSSSSSSSSzzHHHHHHHHHddddddDDDzzzhhhhhfffffffffftttttteeeeeeeeEEEEEEEEEaaaaaaccccccccccFFFFFFFF";
+  // string out = obj.zipString(in);
+  // string expect = "j6x7o2Z1L8Q9L8E1C1X7I9h1j2y3S2o9m3u2E9n7f6A5l8b4k12K6h9o10Y1C6O9M10i6v7W4k6w7M1m8L3w7k3j6t4Q9a7F7l9g10P6u8a1Y6w1Q3F10a5p1X7x6Q9s5G4f7d6p3Q6H3T3a6G6d2y5M1h2l10N8U3W6L9Y10T2K8i7t7X9L3H1Z7s4j2J3E5O5t10B2t6T10r4t4R5y2o6a9r7P7j2P4d10H4n10S10z2H9d6D3z3h5f10t6e8E9a6c10F8";
+  // if (out == expect) {
+    // cout << "case5: check ok. with input:" << in << endl;
+  // } else {
+    // cout << "case5: check failed. with input:" << in << "\nexpect output:" << expect << "\nactual output:" << out << endl;
+  // }
 
-  return 0;
-}
+  // return 0;
+// }
