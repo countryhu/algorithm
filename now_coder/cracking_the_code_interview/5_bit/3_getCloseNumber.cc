e6b307e4f8ad4f6c02cf0806f413a1a33993f55a
countryhu <countryhu@163.com>
2017-10-25 10:49:17 +0800

    3_getCloseNumber

--- now_coder/cracking_the_code_interview/5_bit/3_getCloseNumber.cc
+++ now_coder/cracking_the_code_interview/5_bit/3_getCloseNumber.cc
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <bitset>
 #include <vector>
+#include <climits>
 
 using namespace std;
 
@@ -16,51 +17,41 @@ using namespace std;
   -----
 
   题目分析
-  1.
+  1.x 是正整数，可以遍历
  **/
 
 class CloseNumber {
  public:
   vector<int> getCloseNumber(int x) {
     vector<int> rets;
-    // 获取略小值
-    bitset<32> binNumberMin(x);
-    cout << "         x:" << binNumberMin << " " << binNumberMin.to_ulong() << endl;
-    int alreadeHave0Pos = FindFirstToLeft(binNumberMin, 0, 0);
-    int needMove1Pos = FindFirstToLeft(binNumberMin, 1, alreadeHave0Pos + 1);
-    int needMove0Pos = FindFirstToRight(binNumberMin, 0, needMove1Pos - 1);
-    binNumberMin[needMove1Pos] = 0;
-    binNumberMin[needMove0Pos] = 1;
-    cout << "min closer:" << binNumberMin << " " << binNumberMin.to_ulong() << endl;
-    rets.push_back((int)binNumberMin.to_ulong());
+    int xOneBitsCount = OneBitsCount(x);
 
-    // 获取略大值
-    bitset<32> binNumberMax(x);
-    needMove1Pos = FindFirstToLeft(binNumberMax, 1, 0);
-    needMove0Pos = FindFirstToLeft(binNumberMax, 0, needMove1Pos + 1);
-    binNumberMax[needMove1Pos] = 0;
-    binNumberMax[needMove0Pos] = 1;
-    cout << "max closer:" << binNumberMax << " " << binNumberMax.to_ulong() << endl;
-    rets.push_back((int)binNumberMax.to_ulong());
+    // 找较小值
+    for (int closeLess = x - 1; closeLess >= 0; --closeLess) {
+      if (OneBitsCount(closeLess) == xOneBitsCount) {
+        rets.push_back(closeLess);
+        break;
+      }
+    }
+
+    // 找较大值
+    for (int closeBigger = x + 1; closeBigger <= INT_MAX; ++closeBigger) {
+      if (OneBitsCount(closeBigger) == xOneBitsCount) {
+        rets.push_back(closeBigger);
+        break;
+      }
+    }
     return rets;
   }
 
  private:
-  int FindFirstToLeft(const bitset<32>& bitNum, bool findVal, int startPos) {
-    for (int i = startPos; i < 32; ++i) {
-      if (bitNum[i] == findVal) {
-        return i;
-      }
+  int OneBitsCount(int x) {
+    int count = 0;
+    while (x > 0) {
+      x = x & (x-1);
+      count ++;
     }
-    return -1;
-  }
-  int FindFirstToRight(const bitset<32>& bitNum, bool findVal, int startPos) {
-    for (int i = startPos; i >= 0; --i) {
-      if (bitNum[i] == findVal) {
-        return i;
-      }
-    }
-    return -1;
+    return count;
   }
 };
 
@@ -68,9 +59,14 @@ int main() {
   CloseNumber obj;
   obj.getCloseNumber(2);
   obj.getCloseNumber(2034);
-  obj.getCloseNumber(76351);
 
-  cout << "max c76284:" << bitset<32>(76284) << endl;
+  std::vector<int> rets = obj.getCloseNumber(76351);
+  cout << "val c76351:" << bitset<32>(76351) << endl;
+
+  cout << "min c76284:" << bitset<32>(rets[0]) << endl;
+  cout << "min c76284:" << bitset<32>(76284) << endl;
+
+  cout << "max c76284:" << bitset<32>(rets[1]) << endl;
   cout << "max c76383:" << bitset<32>(76383) << endl;
   return 0;
 }
